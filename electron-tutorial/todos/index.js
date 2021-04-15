57e0b367abc4a990cceac3ec591411b9491f0e02
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2021-04-16 01:07:45 +0900

    [튜토리얼] Electron JS Tutorial - Todo App
    - todo list 초기화 기능을 가진 메뉴 생성
    - 해당 메뉴 선택시 ipcRenderer로 이벤트 전송 -> html에서 받아서 todo list 지우는 작업 실행

--- electron-tutorial/todos/index.js
+++ electron-tutorial/todos/index.js
@@ -53,6 +53,12 @@ const menuTemplate = [
 				},
 			},
 			{
+				label: 'Clear Todos',
+				click() {
+					mainWindow.webContents.send('todo:clear');
+				},
+			},
+			{
 				label: 'Quit',
 				accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
 				click() {
